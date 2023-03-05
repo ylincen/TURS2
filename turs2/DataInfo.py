b1267f5c63adf0e9b75eb750a74de2ac0452b8d4
Yang <l.yang@lumc.nl>
2023-03-05 12:31:04 +0100

    refactoring

--- turs2/DataInfo.py
+++ turs2/DataInfo.py
@@ -4,41 +4,7 @@ import numpy as np
 from constant import *
 from math import log
 from scipy.special import gammaln, comb
-
-
-def log2comb(n, k):
-    return (gammaln(n+1) - gammaln(n-k+1) - gammaln(k+1)) / log(2)
-
-
-def universal_code_integers_maximum(n: int, maximum : int) -> float:
-    """ computes the universal code of integers when there is a known maximum integer
-    This is equivalent to applying the maximum entropy principle knowing the maximum,
-    and it equalitarian  division of the non-used probability (the ones after the maximum)
-    by all the used number (1 until maximum).
-    """
-    probability_until_max = np.sum([2**-universal_code_integers(n_aux) for n_aux in range(1, maximum+1)])
-    probability_left = 1 - probability_until_max
-    probability_n = 2**-universal_code_integers(n) + probability_left/maximum
-    logsum = -np.log2(probability_n)
-    return logsum
-
-
-def universal_code_integers(value: int) -> float:
-    """ computes the universal code of integers
-    """
-    const = 2.865064
-    logsum = np.log2(const)
-    if value == 0:
-        logsum = 0
-    elif value > 0:
-        while True: # Recursive log
-            value = np.log2(value)
-            if value < 0.001:
-                break
-            logsum += value
-    elif value < 0:
-        raise ValueError('n should be larger than 0. The value was: {}'.format(value))
-    return logsum
+from turs2.utils_modelencoding import *
 
 
 class DataInfo:
