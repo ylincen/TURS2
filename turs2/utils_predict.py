1cad7231506978de439987b458f93dc990a5c805
Yang <l.yang@lumc.nl>
2023-03-20 15:06:21 +0100

    change the prediction function, so that if a data point is covered by multiple rules but not by a single modeling_group, we can still handle it

--- turs2/utils_predict.py
+++ turs2/utils_predict.py
@@ -2,16 +2,17 @@ import numpy as np
 from turs2.utils_calculating_cl import *
 
 
-def predict_ruleset(ruleset, X_test, y_test):
+def get_rule_local_prediction_for_unseen_data(ruleset, X_test, y_test):
     if type(X_test) != np.ndarray:
         X_test = X_test.to_numpy()
 
     if type(y_test) != np.ndarray:
         y_test = y_test.to_numpy().flatten()
-    prob_predicted = np.zeros((len(X_test), ruleset.data_info.num_class), dtype=float)
     rules_test_p = []
-    covered_all_rules = np.zeros(len(X_test), dtype=bool)  # for calculating the else_rule's cover
+    rules_test_coverage = []
 
+    allrules_cover = np.zeros(len(X_test), dtype=bool)
+    rule_cover_test = []
     for rule in ruleset.rules:
         condition_matrix = np.array(rule.condition_matrix)
         condition_count = np.array(rule.condition_count)
@@ -25,10 +26,34 @@ def predict_ruleset(ruleset, X_test, y_test):
         for v in which_vars:
             rule_cover = rule_cover & (X_test[:, v] < upper_bound[v]) & (X_test[:, v] >= lower_bound[v])
 
+        rule_cover_test.append(rule_cover)
         # only for comparison with the estimated probability from the training set, not for evaluating the model!!!
         rule_test_p = calc_probs(y_test[rule_cover], ruleset.data_info.num_class)
         rules_test_p.append(rule_test_p)
+        rules_test_coverage.append(np.count_nonzero(rule_cover))
 
+        allrules_cover = np.bitwise_or(allrules_cover, rule_cover)
+    else_rule_cover = ~allrules_cover
+    else_rule_coverage = np.count_nonzero(else_rule_cover)
+    else_rule_p = calc_probs(y_test[else_rule_cover], ruleset.data_info.num_class)
+    return {"rules_test_p": rules_test_p, "rules_test_coverage": rules_test_coverage,
+            "else_rule_p": else_rule_p, "else_rule_coverage": else_rule_coverage,
+            "allrules_cover": allrules_cover, "rule_cover_test": rule_cover_test}
+
+
+def predict_ruleset(ruleset, X_test, y_test):
+    if type(X_test) != np.ndarray:
+        X_test = X_test.to_numpy()
+
+    if type(y_test) != np.ndarray:
+        y_test = y_test.to_numpy().flatten()
+    prob_predicted = np.zeros((len(X_test), ruleset.data_info.num_class), dtype=float)
+
+    rules_test_res = get_rule_local_prediction_for_unseen_data(ruleset, X_test, y_test)
+    rules_test_p = rules_test_res["rules_test_p"]
+    allrules_cover = rules_test_res["allrules_cover"]
+
+    allmgs_cover = np.zeros(len(X_test), dtype=bool)
     for mg in ruleset.modelling_groups:
         mg_cover = np.ones(len(X_test), dtype=bool)
         for rule_index in mg.rules_involvde:
@@ -45,14 +70,27 @@ def predict_ruleset(ruleset, X_test, y_test):
                 mg_cover = mg_cover & (X_test[:, v] < upper_bound[v]) & (X_test[:, v] >= lower_bound[v])
 
         prob_predicted[mg_cover] = mg.prob_model
-        covered_all_rules = np.bitwise_or(covered_all_rules, mg_cover)
+        allmgs_cover = np.bitwise_or(allmgs_cover, mg_cover)
 
-    prob_predicted[~covered_all_rules] = ruleset.else_rule_p
-    if any(~covered_all_rules):
-        rules_test_p.append(calc_probs(y_test[~covered_all_rules], ruleset.data_info.num_class))
-    else:
-        rules_test_p.append([0, 0])
-    return [prob_predicted, rules_test_p, ~covered_all_rules]
+    not_covered_by_all_mgs = np.bitwise_and(allrules_cover, ~allmgs_cover)
+    rule_cover_test = rules_test_res["rule_cover_test"]
+    if any(not_covered_by_all_mgs):
+        not_covered_by_all_mgs_indices = np.where(not_covered_by_all_mgs)[0]
+        weighted_p = 0
+        count = 0
+        for i in not_covered_by_all_mgs_indices:
+            rules_involve_bool = np.zeros(len(ruleset.rules), dtype=bool)
+            for j, rule_j_cover in enumerate(rule_cover_test):
+                if rule_j_cover[i]:
+                    rules_involve_bool[j] = True
+                    weighted_p += ruleset.rules[j].prob * ruleset.rules[j].coverage
+                    count += ruleset.rules[j].coverage
+            weighted_p = weighted_p / count
+            prob_predicted[i] = weighted_p
+
+    prob_predicted[~rules_test_res["allrules_cover"]] = ruleset.else_rule_p
+
+    return [prob_predicted, rules_test_p, ~allrules_cover]
 
 
 def predict_rulelist(ruleset, X_test, y_test):
