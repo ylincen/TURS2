aced6db27eb30508898176a0cb5ce80911c389c8
Yang <l.yang@lumc.nl>
2023-03-21 15:07:56 +0100

    several changes; now working!!

--- turs2/test_ruleset_icu.py
+++ turs2/test_ruleset_icu.py
@@ -16,13 +16,19 @@ from turs2.DataEncoding import *
 X = pd.read_csv(r'\\vf-DataSafe\DataSafe$\div0\ITenDI\Heropname_1136\Files_Lincen_Siri\Processed datasets\2020\Readmission\X_train_StandardScaler_meanimputation_missing_features_dropped.csv')
 # y = pd.read_csv(r'\\vf-DataSafe\DataSafe$\div0\ITenDI\Heropname_1136\Files_Lincen_Siri\Processed datasets\2020\Readmission\y_train.csv')
 colnames = X.columns
+# X_test = pd.read_csv(r'\\vf-DataSafe\DataSafe$\div0\ITenDI\Heropname_1136\Files_Lincen_Siri\Processed datasets\2020\Readmission\X_test_StandardScaler_meanimputation_missing_features_dropped.csv')
+# y_test = pd.read_csv(r'\\vf-DataSafe\DataSafe$\div0\ITenDI\Heropname_1136\Files_Lincen_Siri\Processed datasets\2020\Readmission\y_test.csv')
+
 
 X = pd.read_csv(r'\\vf-DataSafe\DataSafe$\div0\ITenDI\Heropname_1136\Files_Lincen_Siri\Processed datasets\2020\Readmission\X_train_no_scale.csv')
 X = X.loc[:, colnames]
 y = pd.read_csv(r'\\vf-DataSafe\DataSafe$\div0\ITenDI\Heropname_1136\Files_Lincen_Siri\Processed datasets\2020\Readmission\y_train.csv')
+X_test = pd.read_csv(r'\\vf-DataSafe\DataSafe$\div0\ITenDI\Heropname_1136\Files_Lincen_Siri\Processed datasets\2020\Readmission\X_test_no_scale.csv')
+X_test = X_test.loc[:, colnames]
+y_test = pd.read_csv(r'\\vf-DataSafe\DataSafe$\div0\ITenDI\Heropname_1136\Files_Lincen_Siri\Processed datasets\2020\Readmission\y_test.csv')
 
-# X = X.iloc[:, :10]
-
+X = X.iloc[:, :10]
+X_test = X_test.iloc[:, :10]
 
 beamwidth = 1
 data_info = DataInfo(X=X, y=y, num_candidate_cuts=20, max_rule_length=5, feature_names=X.columns, beam_width=1)
@@ -32,15 +38,6 @@ model_encoding = ModelEncodingDependingOnData(data_info)
 ruleset = Ruleset(data_info=data_info, data_encoding=data_encoding, model_encoding=model_encoding)
 
 ruleset.fit(max_iter=1000)
-
-
-# X_test = pd.read_csv(r'\\vf-DataSafe\DataSafe$\div0\ITenDI\Heropname_1136\Files_Lincen_Siri\Processed datasets\2020\Readmission\X_test_StandardScaler_meanimputation_missing_features_dropped.csv')
-# y_test = pd.read_csv(r'\\vf-DataSafe\DataSafe$\div0\ITenDI\Heropname_1136\Files_Lincen_Siri\Processed datasets\2020\Readmission\y_test.csv')
-X_test = pd.read_csv(r'\\vf-DataSafe\DataSafe$\div0\ITenDI\Heropname_1136\Files_Lincen_Siri\Processed datasets\2020\Readmission\X_test_no_scale.csv')
-X_test = X_test.loc[:, colnames]
-y_test = pd.read_csv(r'\\vf-DataSafe\DataSafe$\div0\ITenDI\Heropname_1136\Files_Lincen_Siri\Processed datasets\2020\Readmission\y_test.csv')
-
-
 res = predict_ruleset(ruleset, X_test, y_test)
 
 # readable = get_readable_rules(ruleset)
@@ -49,7 +46,15 @@ roc_auc = roc_auc_score(y_test, res[0][:, 1])
 pr_curve = precision_recall_curve(y_test, res[0][:, 1])
 print(auc(pr_curve[1], pr_curve[0]))
 
-print(roc_auc)
+print("roc_auc on test data: ", roc_auc)
+print("precision-recall auc on test data: ", auc(pr_curve[1], pr_curve[0]))
+
+train_res = predict_ruleset(ruleset, X, y)
+roc_auc_train = roc_auc_score(y, train_res[0][:, 1])
+pr_curve_train = precision_recall_curve(y, train_res[0][:, 1])
+
+print("roc_auc on training data: ", roc_auc_train)
+print("precision-recall auc on training data: ", auc(pr_curve_train[1], pr_curve_train[0]))
 
 covered = (res[0][:, 0] != ruleset.else_rule_p[0])
 roc_auc_score(y_test[covered], res[0][covered, 1])
@@ -57,6 +62,12 @@ roc_auc_score(y_test[covered], res[0][covered, 1])
 for i, r in enumerate(ruleset.rules):
     print(r.prob_excl, r.prob, res[1][i], r.coverage_excl, r.coverage)
 
+new_ruleset = ruleset.modify_rule(1, [1])
+pred_new_ruleset = predict_ruleset(new_ruleset, X_test, y_test)
+roc_auc_score(y_test, pred_new_ruleset[0][:, 1])
+
+
+# Benchmark against RF and also make a hybrid model with it.
 rf = RandomForestClassifier()
 rf.fit(X[ruleset.uncovered_bool], y[ruleset.uncovered_bool].to_numpy().flatten())
 ypred_prob2 = rf.predict_proba(X_test[res[2]])
