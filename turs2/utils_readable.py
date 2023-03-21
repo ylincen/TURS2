6cbabd1fca0430864de4de08e0f13f439c0c6e04
Yang <l.yang@lumc.nl>
2023-03-21 15:48:33 +0100

    change the order of probs (before is wrong); change prob_excl to prob for each rule (before is wrong); make the binary variable more readable

--- turs2/utils_readable.py
+++ turs2/utils_readable.py
@@ -8,17 +8,22 @@ def get_readable_rule(rule):
     for v in which_variables:
         cut = rule.condition_matrix[:, v][::-1]
         icol_name = feature_names[v]
-        # readable += "X" + str(v) + "-" + icol_name + " in " + str(cut) + ";   "
         if np.isnan(cut[0]):
-            cut_condition = "(X" + str(v) + ") " + icol_name + " < " + str(cut[1]) + ";   "
+            if cut[1] == 0.5 and len(rule.data_info.candidate_cuts[v]) == 1:
+                cut_condition = "(X" + str(v) + "[binary variable]) " + icol_name + " = " + "0" + ";   "
+            else:
+                cut_condition = "(X" + str(v) + ") " + icol_name + " < " + str(cut[1]) + ";   "
         elif np.isnan(cut[1]):
-            cut_condition = "(X" + str(v) + ") " + icol_name + " >= " + str(cut[0]) + ";   "
+            if cut[0] == 0.5 and len(rule.data_info.candidate_cuts[v]) == 1:
+                cut_condition = "(X" + str(v) + "[binary variable]) " + icol_name + " = " + "1" + ";   "
+            else:
+                cut_condition = "(X" + str(v) + ") " + icol_name + " >= " + str(cut[0]) + ";   "
         else:
             cut_condition = str(cut[0]) + " <=    " + "(X" + str(v) + ") " + icol_name + " < " + str(cut[1]) + ";   "
         readable += cut_condition
     readable = "If  " + readable
 
-    readable += "\nProbability of READMISSION or NOT (in order): " + str(rule.prob_excl) + "\nNumber of patients who satisfy this rule: " + str(rule.coverage_excl) + "\n"
+    readable += "\nProbability of READMISSION or NOT (in order): " + str(rule.prob[::-1]) + "\nNumber of patients who satisfy this rule: " + str(rule.coverage_excl) + "\n"
     return(readable)
 
 
@@ -28,6 +33,6 @@ def get_readable_rules(ruleset):
         readable = get_readable_rule(rule)
         readables.append(readable)
         print(readable)
-    readable = "If none of above,\nProbability of READMISSION or NOT (in order): " + str(ruleset.else_rule_p) + "\nNumber of patients who do not satisfy any above rules: : " + str(ruleset.else_rule_coverage)
+    readable = "If none of above,\nProbability of READMISSION or NOT (in order): " + str(ruleset.else_rule_p[::-1]) + "\nNumber of patients who do not satisfy any above rule: " + str(ruleset.else_rule_coverage)
     print(readable)
     readables.append(readable)
