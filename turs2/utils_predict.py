12d17968eb388c9987af3636036b9fb1d285a91a
Yang <l.yang@lumc.nl>
2023-06-21 13:39:28 +0200

    fix a huge bug

--- turs2/utils_predict.py
+++ turs2/utils_predict.py
@@ -72,12 +72,70 @@ def predict_ruleset(ruleset, X_test, y_test):
 
     if type(y_test) != np.ndarray:
         y_test = y_test.to_numpy().flatten()
+
+    prob_predicted = np.zeros((len(X_test), ruleset.data_info.num_class), dtype=float)
+    cover_matrix = np.zeros((len(X_test), len(ruleset.rules) + 1), dtype=bool)
+
+    test_uncovered_bool = np.ones(len(X_test), dtype=bool)
+    for ir, rule in enumerate(ruleset.rules):
+        r_bool_array = np.ones(len(X_test), dtype=bool)
+
+        condition_matrix = np.array(rule.condition_matrix)
+        condition_count = np.array(rule.condition_count)
+        which_vars = np.where(condition_count > 0)[0]
+
+        upper_bound, lower_bound = condition_matrix[0], condition_matrix[1]
+        upper_bound[np.isnan(upper_bound)] = np.Inf
+        lower_bound[np.isnan(lower_bound)] = -np.Inf
+
+        for v in which_vars:
+            r_bool_array = r_bool_array & (X_test[:, v] < upper_bound[v]) & (X_test[:, v] >= lower_bound[v])
+
+        cover_matrix[:, ir] = r_bool_array
+        test_uncovered_bool = test_uncovered_bool & ~r_bool_array
+    cover_matrix[:, -1] = test_uncovered_bool
+
+    cover_matrix_int = cover_matrix.astype(int)
+    unique_id = np.zeros(len(X_test), dtype=int)
+    for kol in range(cover_matrix_int.shape[1]):
+        unique_id += 2 ** kol * cover_matrix_int[:, kol]
+
+    groups, ret_index = np.unique(unique_id, return_index=True)
+    unique_id_dir = {}
+    for g, rind in zip(groups, ret_index):
+        unique_id_dir[g] = cover_matrix_int[rind]
+
+    unique_id_prob_dir = {}
+    for z, t in unique_id_dir.items():
+        bool_model = np.zeros(len(ruleset.data_info.target), dtype=bool)
+        for i_tt, tt in enumerate(t):
+            if tt == 1:
+                if i_tt == len(ruleset.rules):
+                    bool_model = ruleset.uncovered_bool
+                else:
+                    bool_model = np.bitwise_or(bool_model, ruleset.rules[i_tt].bool_array)
+        unique_id_prob_dir[z] = calc_probs(ruleset.data_info.target[bool_model],
+                                           ruleset.data_info.num_class)
+
+    for i in range(len(prob_predicted)):
+        prob_predicted[i] = unique_id_prob_dir[unique_id[i]]
+
+    return prob_predicted
+
+
+def predict_ruleset_wrong(ruleset, X_test, y_test):
+    if type(X_test) != np.ndarray:
+        X_test = X_test.to_numpy()
+
+    if type(y_test) != np.ndarray:
+        y_test = y_test.to_numpy().flatten()
     prob_predicted = np.zeros((len(X_test), ruleset.data_info.num_class), dtype=float)
 
     rules_test_res = get_rule_local_prediction_for_unseen_data(ruleset, X_test, y_test)
     rules_test_p = rules_test_res["rules_test_p"]
     allrules_cover = rules_test_res["allrules_cover"]
 
+    # TODO: BELOW IS F**KING WRONG
     allmgs_cover = np.zeros(len(X_test), dtype=bool)
     for mg in ruleset.modelling_groups:
         mg_cover = np.ones(len(X_test), dtype=bool)
