83a45eb98aacf3abf0d969aee9b840d72bde2189
Lincen's MacbookPro <l.yang@liacs.leidenuniv.nl>
2023-07-11 11:19:41 +0200

    use namedtuple for parameter management

--- turs2/DataInfo.py
+++ turs2/DataInfo.py
@@ -8,29 +8,25 @@ from math import log
 from scipy.special import gammaln, comb
 from turs2.utils_modelencoding import *
 from turs2.utils_calculating_cl import *
+from turs2.utils_alg_config import AlgConfig
 
 from datetime import datetime
 import os
 
 
 class DataInfo:
-    def __init__(self, X, y, beam_width, alg_config=None):
-        AlgConfig = namedtuple('AlgConfig', [
-            "beam_width",
-            "num_candidate_cuts", "max_num_rules", "max_grow_iter",
-            "num_class_as_given",
-            "dataset_name", "feature_names",
-            "rf_assist", "rf_oob_decision_function",
-            "log_learning_process", "X_test", "y_test",
-        ])
-
+    def __init__(self, X, y, beam_width, alg_config=None, log_folder_name="log_withoutDateTime"):
         if alg_config is None:
             alg_config = AlgConfig(
-                num_candidate_cuts=100, max_num_rules=500, max_grow_iter=500, num_class_as_given=None,
+                num_candidate_cuts=100, max_num_rules=500, max_grow_iter=200, num_class_as_given=None,
                 beam_width=beam_width,
-                log_learning_process=False, dataset_name=None, X_test=None, y_test=None, # X_test, y_test only for logging
+                log_learning_process=False, log_folder_name=log_folder_name,
+                dataset_name=None, X_test=None, y_test=None, # X_test, y_test only for logging
                 rf_assist=False, rf_oob_decision_function=None,
-                feature_names=["X" + str(i) for i in range(X.shape[1])]
+                feature_names=["X" + str(i) for i in range(X.shape[1])],
+                beamsearch_positive_gain_only=False, beamsearch_normalized_gain_must_increase_comparing_rulebase=False,
+                beamsearch_stopping_when_best_normalized_gain_decrease=False,
+                validity_check="incl_check", rerun_on_invalid=False, rerun_positive_control=False
             )
             self.alg_config = alg_config
         else:
@@ -45,7 +41,9 @@ class DataInfo:
             self.target = y.to_numpy().flatten()
         else:
             self.target = y
-
+        if self.alg_config.log_learning_process:
+            self.log_folder_name = self.alg_config.log_folder_name
+            os.makedirs(self.alg_config.log_folder_name, exist_ok=True)
         self.max_rule_length = self.alg_config.max_grow_iter  # TODO: the name max_rule_length is misleading
         self.nrow, self.ncol = X.shape[0], X.shape[1]
 
