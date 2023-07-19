bd814f2d619418c33b03622284fb5adb71426802
Lincen's MacbookPro <l.yang@liacs.leidenuniv.nl>
2023-07-19 15:19:27 +0200

    add bash scripts

--- turs2/run_uci_small.sh
+++ turs2/run_uci_small.sh
@@ -3,7 +3,7 @@
 # Define array of data names for the first python script
 
 data_names1=("chess" "iris" "backnote" "contracept" "ionosphere" "car" "tic-tac-toe" "wine" "diabetes")
-data_names1=("iris" "wine")
+# data_names1=("iris" "wine")
 
 # Run the script for each data name in the first list
 for data_name in "${data_names1[@]}"; do
