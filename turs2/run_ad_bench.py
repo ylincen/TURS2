5bacd717d281479dbff8c48d982eaa6174317a28
L. Yang <l.yang@liacs.leidenuniv.nl>
2023-07-28 15:15:03 +0200

    implement run_ad_bench

--- turs2/run_ad_bench.py
+++ turs2/run_ad_bench.py
@@ -1,15 +1,25 @@
 import sys
-# sys.path.extend(['~/projects/TURS'])
-sys.path.extend(['/Users/yanglincen/projects/TURS'])
 
-import os
+# for mac local
+sys.path.extend(['/Users/yanglincen/projects/TURS'])
+sys.path.extend(['/Users/yanglincen/projects/TURS/turs2'])
+# for DSlab server:
+sys.path.extend(['/home/yangl3/projects/turs'])
+sys.path.extend(['/home/yangl3/projects/turs/turs2'])
+
+import numpy as np
+import pandas as pd
 import copy
 import time
+import cProfile
+from datetime import datetime
+from line_profiler import LineProfiler
+import cProfile, pstats
 
-import pandas as pd
 from sklearn.preprocessing import LabelEncoder, OrdinalEncoder, OneHotEncoder
-from sklearn.metrics import roc_auc_score, precision_recall_curve, f1_score, auc
+from sklearn.metrics import roc_auc_score, precision_recall_curve, f1_score, auc, log_loss
 from sklearn.model_selection import StratifiedKFold
+from sklearn.ensemble import RandomForestClassifier
 
 from turs2.DataInfo import *
 from turs2.Ruleset import *
@@ -17,93 +27,63 @@ from turs2.utils_predict import *
 from turs2.ModelEncoding import *
 from turs2.DataEncoding import *
 
-data_names = os.listdir("../ADbench_datasets_Classical")
-data_names_selected = []
-for data_name in data_names:
-    d = np.load("../ADbench_datasets_Classical/" + data_name)
-    X = d["X"]
-    y = d["y"]
-    num_class = len(np.unique(y))
-    # print(num_class)
-    y_prob = calc_probs(y, num_class)
-    if min(y_prob) > 0.05:
-        continue
-    elif len(y) > 1e5:
-        continue
-    else:
-        data_names_selected.append(data_name)
+from turs2.exp_utils import *
+from turs2.exp_predictive_perf import *
 
-### GLOBAL OPTION ###
-trial_run = False
-add_noise = True
+np.seterr(all='raise')
 
-time_all_data = []
-auc_all_data = []
-F1_score_all_data = []
-PR_auc_all_data = []
-data_name_all_data = []
+exp_res_alldata = []
+date_and_time = datetime.now().strftime("%Y%m%d_%H%M%S")
 
-for data_name in data_names_selected:
-    if data_name != "34_smtp.npz":
-        continue
-    print("Running on: ", data_name)
-    d = np.load("../ADbench_datasets_Classical/" + data_name)
-    X = d["X"]
-    y = d["y"]
-    if trial_run:
-        rows = np.random.choice(np.arange(len(X)), size=min(1000, len(X)), replace=False)
-        cols = np.random.choice(np.arange(X.shape[1]), size=min(5, X.shape[1]), replace=False)
-        X = X[rows][:, cols]
-        y = y[rows]
-    num_class = len(np.unique(y))
-
-    skf = StratifiedKFold(n_splits=5, shuffle=True,
-                         random_state=1)  # can also use sklearn.model_selection.StratifiedKFold
-    if add_noise:
-        np.random.seed(0)
-        corrupted_indices = np.random.choice(np.arange(len(y)), size=int(0.2 * len(y)), replace=False)
-        y[corrupted_indices] = (~y[corrupted_indices].astype(bool)).astype(int)
-
-    for i, (train_index, test_index) in enumerate(skf.split(X, y)):
-        X_train, y_train = X[train_index], y[train_index]
-        X_test, y_test = X[test_index], y[test_index]
-
-        feature_names = ["X" + str(i) for i in range(X_train.shape[1])]
-        data_info = DataInfo(X=X_train, y=y_train, num_candidate_cuts=500, max_rule_length=10,
-                             feature_names=feature_names, beam_width=1)
-
-        time0 = time.time()
-        data_encoding = NMLencoding(data_info)
-        model_encoding = ModelEncodingDependingOnData(data_info)
-        ruleset = Ruleset(data_info=data_info, data_encoding=data_encoding, model_encoding=model_encoding)
-
-        ruleset.fit(max_iter=1000, printing=True)
-        res = predict_ruleset(ruleset, X_test, y_test)
-
-        if len(np.unique(y)) == 2:
-            roc_auc = roc_auc_score(y_test, res[0][:, 1])
+if len(sys.argv) == 1:
+    data_names = os.listdir("../ADbench_datasets_Classical")
+    data_names_selected = []
+    for data_name in data_names:
+        d = np.load("../ADbench_datasets_Classical/" + data_name)
+        X = d["X"]
+        y = d["y"]
+        num_class = len(np.unique(y))
+        # print(num_class)
+        y_prob = calc_probs(y, num_class)
+        if min(y_prob) > 0.05:
+            continue
+        elif len(y) > 1e5:
+            continue
         else:
-            roc_auc = roc_auc_score(y_test, res[0], multi_class="ovr")
-        runtime = time.time() - time0
+            data_names_selected.append(data_name)
+    data_name = data_names_selected[0]
+else:
+    data_name = sys.argv[1]
 
-        pr = precision_recall_curve(y_test, res[0][:, 1])  # all ad bench datasets are binary!
-        pr_auc = auc(pr[1], pr[0])
+print("Running on: ", data_name)
+d_np = np.load("../ADbench_datasets_Classical/" + data_name)
+X = d_np["X"]
+y = d_np["y"]
+d_original = pd.DataFrame(np.concatenate([X, y.reshape(-1, 1)], axis=1))
+d = preprocess_data(d_original)
 
-        f1 = f1_score(y_test, res[0][:, 1] > res[0][:, 0])
-        print("roc_auc: ", roc_auc)
+num_class = len(np.unique(y))
+skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=2)
 
-        time_all_data.append(runtime)
-        auc_all_data.append(roc_auc)
-        F1_score_all_data.append(f1)
-        PR_auc_all_data.append(pr_auc)
-        data_name_all_data.append(data_name)
+for fold, (train_index, test_index) in enumerate(skf.split(X, y)):
+    X_train, y_train = X[train_index], y[train_index]
+    X_test, y_test = X[test_index], y[test_index]
 
-        res_pd = pd.DataFrame({
-            "data": data_name_all_data,
-            "time": time_all_data,
-            "roc_auc": auc_all_data,
-            "f1": F1_score_all_data,
-            "pr_auc": PR_auc_all_data
-        })
+    start_time = time.time()
+    data_info = DataInfo(X=X_train, y=y_train, beam_width=20)
 
-        # res_pd.to_csv('res.csv')
+    data_encoding = NMLencoding(data_info)
+    model_encoding = ModelEncodingDependingOnData(data_info)
+    ruleset = Ruleset(data_info=data_info, data_encoding=data_encoding, model_encoding=model_encoding)
+    ruleset.fit(max_iter=1000, printing=True)
+    end_time = time.time()
+    exp_res = calculate_exp_res(ruleset, X_test, y_test, X_train, y_train, data_name, fold, start_time, end_time)
+    exp_res_alldata.append(exp_res)
+exp_res_df = pd.DataFrame(exp_res_alldata)
+
+folder_name = "exp_adbench_" + date_and_time[:8]
+os.makedirs(folder_name, exist_ok=True)
+res_file_name = "./" + folder_name + "/" + date_and_time + "_" + data_name + "_adbench_datasets_res.csv"
+
+exp_res_df.to_csv(res_file_name, index=False)
+
