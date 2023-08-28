7eb59042976db32871a7edd7ea84e54ac6d82fc9
L. Yang <l.yang@liacs.leidenuniv.nl>
2023-08-28 11:37:45 +0200

    update

--- turs2/run_uci.sh
+++ turs2/run_uci.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-data_names0=("Vehicle" "DryBeans" "glass" "pendigits" "HeartCleveland")
+data_names0=("Vehicle" "glass" "pendigits" "HeartCleveland")
 
 fold_numbers=(0 1 2 3 4)
 
@@ -12,27 +12,27 @@ done
 
 wait
 
-## Define array of data names for the first python script
-#data_names1=("chess" "iris" "backnote" "contracept" "ionosphere" "car" "tic-tac-toe" "wine" "diabetes")
-#
-## Run the script for each data name in the first list
-#for data_name in "${data_names1[@]}"; do
-#    python3 run_uci.py "$data_name" &
-#done
-#
-#wait
-#
-#data_names2=("anuran" "avila" "magic" "waveform")
-#
-#fold_numbers=(0 1 2 3 4)
-#
-## Run the script for each data name in the second list, and for each fold number
-#for data_name in "${data_names2[@]}"; do
-#    for fold_number in "${fold_numbers[@]}"; do
-#        python3 run_uci.py "$data_name" "$fold_number" &
-#    done
-#done
-#
-## Wait for all background processes to finish
-#wait
+# Define array of data names for the first python script
+data_names1=("chess" "iris" "backnote" "contracept" "ionosphere" "car" "tic-tac-toe" "wine" "diabetes")
+
+# Run the script for each data name in the first list
+for data_name in "${data_names1[@]}"; do
+    python3 run_uci.py "$data_name" &
+done
+
+wait
+
+data_names2=("anuran" "avila" "magic" "waveform" "DryBeans")
+
+fold_numbers=(0 1 2 3 4)
+
+# Run the script for each data name in the second list, and for each fold number
+for data_name in "${data_names2[@]}"; do
+    for fold_number in "${fold_numbers[@]}"; do
+        python3 run_uci.py "$data_name" "$fold_number" &
+    done
+done
+
+# Wait for all background processes to finish
+wait
 
