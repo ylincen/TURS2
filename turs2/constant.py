3145d87dac4eb4f4a8e309d4af7dab590f4020ae
L. Yang <l.yang@liacs.leidenuniv.nl>
2023-09-04 09:33:43 +0200

    update code; clean constants that are not used

--- turs2/constant.py
+++ turs2/constant.py
@@ -11,14 +11,3 @@ RIGHT_CUT = 1
 # feature type
 CATEGORICAL = 1
 NUMERIC = 0
-
-# for user_defined_subgroup.py
-USER_CONTINUE_REFINE = 0
-USER_NEXT_SUBGROUP = 1
-
-# for mode
-CLASSIFICATION = 0
-SUBDISCOVERY = 1
-
-# for rule growth's coverage reduction constraint
-COV_ALPHA = 0.9
