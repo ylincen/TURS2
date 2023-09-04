db9eb15598356fdcbd94507b25bc1df8c4136c90
L. Yang <l.yang@liacs.leidenuniv.nl>
2023-09-04 16:02:23 +0200

    update

--- turs2/adbench.sh
+++ turs2/adbench.sh
@@ -2,7 +2,7 @@
 
 # Define array of data names for the first python script
 
-data_names1=("26_optdigits.npz" "34_smtp.npz" "28_pendigits.npz" "43_WDBC.npz" "36_speech.npz" "31_satimage-2.npz" "3_backdoor.npz" "38_thyroid.npz" "41_Waveform.npz" "23_mammography.npz" "40_vowels.npz" "25_musk.npz" "1_ALOI.npz" "14_glass.npz")
+data_names1=("26_optdigits.npz" "34_smtp.npz" "28_pendigits.npz" "43_WDBC.npz" "31_satimage-2.npz" "3_backdoor.npz" "38_thyroid.npz" "41_Waveform.npz" "23_mammography.npz" "40_vowels.npz" "25_musk.npz" "1_ALOI.npz" "14_glass.npz")
 
 # Run the script for each data name in the first list
 for data_name in "${data_names1[@]}"; do
